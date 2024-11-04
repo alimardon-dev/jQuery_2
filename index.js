970f71c658e220b95f561f21b7a25dcf02d3d009
Alimardon <155231495+alimardon-dev@users.noreply.github.com>
2024-11-05 03:50:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//changing the attributes
+
+// alert($("img").attr("src"))
+
+// $(document).ready(function(){
+    // alert($("img").attr("src"));
+// })   
+//changing the href attr
+$(document).ready(function(){
+    $("a").attr("href","https://yahoo.com")
+})
+
+
+$(document).ready(function(){
+    // alert($("h1").Class("big-title"));
+    alert($("h1").attr("class"));
+})
